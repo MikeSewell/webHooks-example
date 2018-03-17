ba909d5f83374359a6fca17ce779d203b492c5c7
Michael  Sewell <MikeSewell01@gmail.com>
2018-03-16 21:30:00 -0400

    fixed magic number port

--- src/server.js
+++ src/server.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+
 app.use(bodyParser.json());
 
 app.get('/', (req,res)=>{
 
-    res.status(200).send("<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><meta http-equiv='X-UA-Compatible' content='ie=edge'><title>Document</title></head><body></body></html>")
+    res.status(200).send('Hello world')
 });
 
-app.post('/hook', (req,res)=>{
-// console.log('body ==== : ', req.body)
+app.post('/webhook', (req,res)=>{
     res.status(200).send("hello world")
+    console.log(req.body);
 });
 
 
 var port = process.env.PORT || '3000';
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log("Running on port : ",port);
 
 });
