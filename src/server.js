01d6fd2ac556bb339a103943554d998e4cdc94f1
Michael  Sewell <MikeSewell01@gmail.com>
2018-03-16 20:35:37 -0400

    added body-parser

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.json());
 
@@ -17,3 +18,4 @@ app.listen(3000,()=>{
     console.log("Running on port : ",port);
 
 });
+
